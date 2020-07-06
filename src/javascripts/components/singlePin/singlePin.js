459a83ccf42c63b7605b9a45e546881529811d88
Nikhil Gaikwad <gaikwadrnikhil@gmail.com>
2020-07-05 21:56:06 -0500

    added the detail pin view and got the back button logic working

--- src/javascripts/components/singlePin/singlePin.js
+++ src/javascripts/components/singlePin/singlePin.js
@@ -1,6 +1,6 @@
 const pinBuilder = (pin) => {
   const domString = ` 
-  <div class="card mb-3 hoverEffect align-self-start" id="${pin.id}" style="width: 10rem;">
+  <div class="card mb-3 pinDetail hoverEffect align-self-start" id="${pin.id}" style="width: 10rem;">
   <img class="card-img-top" src="${pin.imgURL}" alt="Card image cap">         
           <div class="d-flex justify-content-between">
           <a href="#" class="btn btn-warning link"><i class="fas fa-external-link-alt"></i></a>
@@ -29,4 +29,32 @@ const allPinBuilder = (pin) => {
   return domString;
 };
 
-export default { pinBuilder, smallPinBuilder, allPinBuilder };
+const pinDetailBuilder = (pin) => {
+  const domString = ` 
+  <div class="d-flex justify-content-center mb-5">
+    <div class="col-8">
+      <div class="card" id="${pin.boardId}">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-6">
+              <div class="card mb-2" style="width: 14rem; border-radius:20px;">
+              <img class="card-img-top" src="${pin.imgURL}" alt="Card image cap">          
+              </div> 
+            </div>
+            <div class="col-6">
+              <a href="#" class="btn btn-outline-dark backButton mb-5"><i class="fas fa-arrow-left"></i></a>
+              <h1>${pin.pinTitle}</h1>
+              <h5>${pin.Description}</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>    
+    `;
+  return domString;
+};
+
+export default {
+  pinBuilder, smallPinBuilder, allPinBuilder, pinDetailBuilder,
+};
